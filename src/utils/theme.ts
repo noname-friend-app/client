d05ea6260da085844b8af712f190fbd9b9c0ede2
Christian Thompson <christianthompson1@hotmail.com>
2023-03-16 20:41:26 -0700

    merge with dev

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
-import "@fontsource/akshar"
+import "@fontsource/akshar";
 
 // 2. Add your color mode config
 const config = {
@@ -9,13 +9,17 @@ const config = {
 
 const colors = {
   purple: {
-    100: '#9D8DF1',
-    200: '#31283C',
-    300: '#271B2D'
-  }
-}
+    100: "#9D8DF1",
+    200: "#31283C",
+    300: "#271B2D",
+  },
+};
 
 // 3. extend the theme
-const theme = extendTheme({ config, colors, fonts: { body: '"Akshar", sans-serif;', heading: '"Akshar", sans-serif;' } });
+const theme = extendTheme({
+  config,
+  colors,
+  fonts: { body: '"Akshar", sans-serif;', heading: '"Akshar", sans-serif;' },
+});
 
 export default theme;
