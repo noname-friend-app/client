653224b781fd515e66e7b9fc7e30dd2e75c5a171
Christian Thompson <christianthompson1@hotmail.com>
2023-03-16 20:30:05 -0700

    preparing for pull

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,15 +1,11 @@
 import { Heading } from "@chakra-ui/react";
 import { Outlet } from "@tanstack/react-router";
-import DoinkButton from "../components/Buton";
-import DoinkInput from "../components/Input";
 
 const Root = () => {
   return (
     <>
       <Heading>Doink!</Heading>
-      {/* <DoinkButton /> */}
-      <DoinkInput />
-      {/* <Outlet /> */}
+      <Outlet />
     </>
   );
 };
