7f7bd8584d81164bd5ccf025561dbfab6fdfbc33
Elias <e.wambugu192@gmail.com>
2023-03-24 13:33:18 -0700

    removed API localhost url

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 axios.defaults.withCredentials = true;
 
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+export const API_URL = process.env.REACT_APP_API_URL
 
 //MUTATIONS
 interface CreateQuoteProps {
