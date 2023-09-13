b52fd24b6f787e14610bde67a255adeefc3aed10
Christian Thompson <ChristianThompson1@hotmail.com>
2023-09-13 13:28:53 -0700

    built out reply button text area

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,25 +1,74 @@
-import { Divider, Flex, HStack, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { useComments } from "../../libs/api";
+import { useParams } from "react-router-dom";
+import Comment from "./Comment";
+import { Flex, Link, Divider, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+import Button from "../Button";
 
-interface Props {
-  text: string;
-  createdAt: string;
-  commentName?: string;
-}
+const Comments = () => {
+  // Comments Logic
+  const [displayTextBox, setDisplayTextBox] = useState<boolean>(false);
+  const { groupId } = useParams();
+  const { quoteId } = useParams();
+  const { data } = useComments({
+    quoteId: quoteId!,
+    groupId: groupId!,
+  });
 
-const Comments: React.FC<Props> = ({ text, createdAt, commentName }) => {
   return (
     <>
-      <Flex  ml={5} flexDir="column">
-        <Flex>
-          <Text fontSize={10}>{createdAt}</Text>
+      <Flex w={"90%"} h={"auto"} flexDir={"column"} ml={5}>
+        {/* {data && data.comments.length > 0 ? (
+          data!.comments.map((comment, index) => (
+            <Comments
+              key={index}
+              text={comment.text}
+              // Ask clarification for ?
+              commentName={comment.profile?.name}
+              createdAt={comment.createdAt}
+            />
+          ))
+        ) : (
+          <></>
+        )} */}
+        <Comment
+          text={"Bro who tf would say that?! XD"}
+          // Ask clarification for ?
+          commentName={"Alex"}
+          createdAt={"10/12/1312"}
+        />
+        <Flex
+          display={displayTextBox ? "flex" : "none"}
+          flexDir={"column"}
+          ml={5}
+        >
+          <Textarea mb={2} placeholder="Add your comment here"/>
+
+          <Button w={"60px"} h={"30px"} >
+            Reply
+          </Button>
+          
         </Flex>
-        <HStack align={"center"} spacing={"4"}>
-          <Text >{commentName}</Text>
-          <Flex w={2} h={2} bg={"white"} borderRadius={"10"} ></Flex>
-          <Text>{text}</Text>
-        </HStack>
+        <Flex ml={5} fontSize={"13px"} mt={2}>
+          <Link
+            onClick={() => setDisplayTextBox(true)}
+            as={RouterLink}
+            color={"purple.100"}
+            pr={2}
+          >
+            Add Comment
+          </Link>
+          <Divider orientation="vertical" pr={2} />
+          <Link as={RouterLink} color={"purple.100"} pr={2}>
+            Show More Comments
+          </Link>
+          <Divider orientation="vertical" pr={2} />
+          <Link as={RouterLink} color={"purple.100"} pr={2}>
+            View All Comments
+          </Link>
+        </Flex>
       </Flex>
-      <Divider mb={2} />
     </>
   );
 };
