063764629287d4d20f311bcfe2a1e2c45091d47e
Christian Thompson <ChristianThompson1@hotmail.com>
2023-09-21 01:33:14 -0700

    comments can be created and display

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,27 +1,56 @@
 import { Link as RouterLink } from "react-router-dom";
-import { useComments } from "../../libs/api";
+import { useComments, useCreateComment } from "../../libs/api";
 import { useParams } from "react-router-dom";
 import Comment from "./Comment";
-import { Flex, Link, Divider, Textarea, Input } from "@chakra-ui/react";
+import { Flex, Link, Divider } from "@chakra-ui/react";
 import { useState } from "react";
 import Button from "../Button";
+import Input from "../Input";
 
-const Comments = () => {
+interface Props {
+  id: string;
+}
+
+const Comments: React.FC<Props> = ({ id }: Props) => {
   // Comments Logic
   const [displayTextBox, setDisplayTextBox] = useState<boolean>(false);
   const { groupId } = useParams();
-  const { quoteId } = useParams();
+  //const quoteid from quote data 
+
   const { data } = useComments({
-    quoteId: quoteId!,
+    quoteId: id,
     groupId: groupId!,
   });
 
+  // console.log(quoteId);
+  // console.log(groupId);
+  // console.log(id);
+
+  const [text, setText] = useState<string>("");
+  const [createdAt, setCreatedAt] = useState<string>("");
+
+  const { mutate } = useCreateComment();
+  // console.log(id);
+
+  // console.log(data)
   return (
     <>
+      {data && data.comments && (
+         <Flex
+         w={10}
+         h={10}
+         ml={5}
+         mb={5}
+         border="1px"
+         borderTop={"none"}
+         borderRight={"none"}
+         borderBottomLeftRadius={"10"}
+       />
+      )}
       <Flex w={"90%"} h={"auto"} flexDir={"column"} ml={5}>
-        {/* {data && data.comments.length > 0 ? (
+        {data && data.comments.length > 0 ? (
           data!.comments.map((comment, index) => (
-            <Comments
+            <Comment
               key={index}
               text={comment.text}
               // Ask clarification for ?
@@ -31,24 +60,36 @@ const Comments = () => {
           ))
         ) : (
           <></>
-        )} */}
-        <Comment
+        )}
+        {/* <Comment
           text={"Bro who tf would say that?! XD"}
           // Ask clarification for ?
           commentName={"Alex"}
           createdAt={"10/12/1312"}
-        />
-        <Flex
-          display={displayTextBox ? "flex" : "none"}
-          flexDir={"row"}
-          ml={5}
-        >
-          <Input variant={'flushed'} mb={2} placeholder="Add your comment here"/>
+        /> */}
+        <Flex display={displayTextBox ? "flex" : "none"} flexDir={"row"} ml={5}>
+          <Input
+            // mb={2}
+            formLabel="Add your comment here"
+            value={text}
+            setState={setText}
+            w={"100%"}
+          />
 
-          <Button w={"60px"} h={"40px"} >
+          <Button
+            w={"60px"}
+            h={"40px"}
+            onClick={() =>
+              mutate({
+                text: text,
+                createdAt,
+                groupId: groupId!,
+                quoteId: id,
+              })
+            }
+          >
             Reply
           </Button>
-          
         </Flex>
         <Flex ml={5} fontSize={"13px"} mt={2}>
           <Link
